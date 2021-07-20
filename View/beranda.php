e739093e8db290f9b2160add3fcdfeeee7a77e8f
khairunisa <khairunisa20ti@mahasiswa.pcr.ac.id>
2021-07-20 23:42:18 +0700

    ProjectUASBPWL

---
+++ View/beranda.php
@@ -0,0 +1,156 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>FAR Roastery</title>
+	<link rel="stylesheet" type="text/css"
+		href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" />
+	<link rel="stylesheet" type="text/css"
+		href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
+	<link rel="stylesheet" type="text/css" href="../Assets/css/style.css" />
+	<script type="text/javascript" src="https://code.jquery.com/jquery-3.4.1.js"></script>
+	<link rel="stylesheet" type="text/css" href="css/util.css">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+
+<body class="bg-dark">
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-md-12 navbar">
+				<a href="far.php" class="logo navbar-brand text-white offset-md-2">D'Kitchen</a>
+				<ul class="nav">
+					<li class="nav-item active"><a href="far.php" class="nav-link">Beranda</a></li>
+					<li class="nav-item"><a href="../index.php" class="nav-link">Log Out</a></li>
+				</ul>
+				<form class="navbar-form navbar-left" action="/action_page.php">
+      <div class="form-group">
+        <input type="text" class="form-control" placeholder="Search">
+      </div>
+      
+    </form>
+			</div>
+<div class="container">
+  <div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+      <li data-target="#myCarousel" data-slide-to="1"></li>
+      <li data-target="#myCarousel" data-slide-to="2"></li>
+    </ol>
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner">
+      <div class="item active">
+        <img src="g1.jpeg" alt="Los Angeles" style="width:100%;">
+      </div>
+
+      <div class="item">
+        <img src="g2.jpeg" alt="Chicago" style="width:100%;">
+      </div>
+    
+      <div class="item">
+        <img src="g3.jpeg" alt="New york" style="width:100%;">
+      </div>
+    </div>
+
+    <!-- Left and right controls -->
+    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+</div>
+    </div>
+    <br>
+	<br>
+    <div class="container">
+        <div class="row">
+          <div class="col-sm">
+              <h2 class="nav-link text-white gmbr" style="text-align : center; margin-left : 0%">CHUROS 😋<br> By Chef Akwacha</h2>
+              <img srcset="../Assets/Image/churos.jpg" class="rounded szfoto" style="margin-left:7%"><br>
+			  <br><h4 style="color: white;font-family:'Poppins', sans-serif;text-align: justify;">Bahan-bahan:</h6>
+			  <br><h4 style="color: white;font-family:'Poppins', sans-serif;text-align: justify;">75 gr butter</h6>
+			  <br><h4 style="color: white;font-family:'Poppins', sans-serif;text-align: justify;">200 ml air</h6>
+			  <br><h4 style="color: white;font-family:'Poppins', sans-serif;text-align: justify;">1,5 sdm gula</h6>
+			  <br><h4 style="color: white;font-family:'Poppins', sans-serif;text-align: justify;">100 gr terigu</h6>
+			  <br><h4 style="color: white;font-family:'Poppins', sans-serif;text-align: justify;">100 gr kentang kukus, haluskan</h6>
+			  <br><h4 style="color: white;font-family:'Poppins', sans-serif;text-align: justify;">2 butir telur</h6>
+          </div>
+          <div class="col-sm">
+		  	  <h2 class="nav-link text-white gmbr" style="text-align : center; margin-left : 0%"">Kerupuk Pangsit<br>By Ilhma</h2>
+              <img srcset="../Assets/Image/pangsit.jpg" class="rounded szfoto" style="margin-left:7%"><br>
+			  <br><h4 style="color: white;font-family:'Poppins', sans-serif;text-align: justify;">Bahan Adonan:</h6>
+			  <br><h4 style="color: white;font-family:'Poppins', sans-serif;text-align: justify;">1 kg Terigu segitiga</h6>
+			  <br><h4 style="color: white;font-family:'Poppins', sans-serif;text-align: justify;">200 g Margarin</h6>
+			  <br><h4 style="color: white;font-family:'Poppins', sans-serif;text-align: justify;">Seledri secukupnya</h6>
+			  <br><h4 style="color: white;font-family:'Poppins', sans-serif;text-align: justify;">Daun Bawang secukupnya </h6>
+			  <br><h4 style="color: white;font-family:'Poppins', sans-serif;text-align: justify;">Penyedap rasa</h6>
+			  <br><h4 style="color: white;font-family:'Poppins', sans-serif;text-align: justify;">1 gelas air </h6>
+          </div>
+          <div class="col-sm">
+			  <h2 class="nav-link text-white gmbr" style="text-align : center; margin-left : 0%"">Kwetiau Goreng Jawa<br>By Sally Febrina</h2>
+              <img srcset="../Assets/Image/kwetiau.jpg" class="rounded szfoto" style="margin-left:7%"><br>
+			  <br><h4 style="color: white;font-family:'Poppins', sans-serif;text-align: justify;">Bahan-bahan:</h6>
+			  <br><h4 style="color: white;font-family:'Poppins', sans-serif;text-align: justify;">200 gr kwetiau kering</h6>
+			  <br><h4 style="color: white;font-family:'Poppins', sans-serif;text-align: justify;">6 cabe rawit</h6>
+			  <br><h4 style="color: white;font-family:'Poppins', sans-serif;text-align: justify;">4 bawang putih cincang</h6>
+			  <br><h4 style="color: white;font-family:'Poppins', sans-serif;text-align: justify;">2 bawang merah iris tipis</h6>
+			  <br><h4 style="color: white;font-family:'Poppins', sans-serif;text-align: justify;">1 daun bawang iris</h6>
+			  <br><h4 style="color: white;font-family:'Poppins', sans-serif;text-align: justify;">6 bakso sapi iris</h6>
+          </div>
+        </div>
+      </div>
+	  <br><br><br>
+	  <div class="container">
+        <div class="row">
+          <div class="col-sm">
+              <h2 class="nav-link text-white gmbr" style="text-align : center; margin-left : 0%">Mi Ayam Jamur Bakso<br>By Andina Ayunani</h2>
+              <img srcset="../Assets/Image/mieayam.jpg" class="rounded szfoto" style="margin-left:7%"><br>
+			  <br><h4 style="color: white;font-family:'Poppins', sans-serif;text-align: justify;">Ingredients:</h6>
+			  <br><h4 style="color: white;font-family:'Poppins', sans-serif;text-align: justify;">500 g chicken boneless thigh</h6>
+			  <br><h4 style="color: white;font-family:'Poppins', sans-serif;text-align: justify;">2 litre water</h6>
+			  <br><h4 style="color: white;font-family:'Poppins', sans-serif;text-align: justify;">1 can sliced champignon mushrooms</h6>
+			  <br><h4 style="color: white;font-family:'Poppins', sans-serif;text-align: justify;">1 onion, minced</h6>
+			  <br><h4 style="color: white;font-family:'Poppins', sans-serif;text-align: justify;">2 cloves garlic, minced</h6>
+			  <br><h4 style="color: white;font-family:'Poppins', sans-serif;text-align: justify;">1 scallion cut small</h6>
+			</div>
+          <div class="col-sm">
+		  	  <h2 class="nav-link text-white gmbr" style="text-align : center; margin-left : 0%"">Siomay<br>By Selena</h2>
+              <img srcset="../Assets/Image/siomay.jpg" class="rounded szfoto" style="margin-left:7%"><br>
+			  <br><h4 style="color: white;font-family:'Poppins', sans-serif;text-align: justify;">Ingredients:</h6>
+			  <br><h4 style="color: white;font-family:'Poppins', sans-serif;text-align: justify;">200g shrimp</h6>
+			  <br><h4 style="color: white;font-family:'Poppins', sans-serif;text-align: justify;">300g chicken meat</h6>
+			  <br><h4 style="color: white;font-family:'Poppins', sans-serif;text-align: justify;">2 tablespoon shallots, grated</h6>
+			  <br><h4 style="color: white;font-family:'Poppins', sans-serif;text-align: justify;">1 tablespoon ground white pepper</h6>
+			  <br><h4 style="color: white;font-family:'Poppins', sans-serif;text-align: justify;">350 gram tapioca starch</h6>
+			  <br><h4 style="color: white;font-family:'Poppins', sans-serif;text-align: justify;">2-3 teaspoon salt</h6>
+          </div>
+          <div class="col-sm">
+			  <h2 class="nav-link text-white gmbr" style="text-align : center; margin-left : 0%"">Nasi Goreng <br>By Bowie</h2>
+              <img srcset="../Assets/Image/nasigoreng.jpg" class="rounded szfoto" style="margin-left:7%"><br>
+			  <br><h4 style="color: white;font-family:'Poppins', sans-serif;text-align: justify;">Bahan-bahan:</h6>
+			  <br><h4 style="color: white;font-family:'Poppins', sans-serif;text-align: justify;">1 porsi nasi putih</h6>
+			  <br><h4 style="color: white;font-family:'Poppins', sans-serif;text-align: justify;">5 siung bawang merah</h6>
+			  <br><h4 style="color: white;font-family:'Poppins', sans-serif;text-align: justify;">3 siung bawang putih</h6>
+			  <br><h4 style="color: white;font-family:'Poppins', sans-serif;text-align: justify;">1 sendok makan udang</h6>
+			  <br><h4 style="color: white;font-family:'Poppins', sans-serif;text-align: justify;">5 cabe rawit </h6>
+			  <br><h4 style="color: white;font-family:'Poppins', sans-serif;text-align: justify;">1 sdm minyak ikan</h6>
+          </div>
+        </div>
+      <footer class="bg-dark">
+        
+
+	<script type="text/javascript" src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js">
+	</script>
+</body>
+
+</html>
